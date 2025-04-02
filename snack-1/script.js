19d65bbfccaf7dc195393d4350f6692030a5aac4
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-02 13:30:47 +0200

    es1 folder 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log('----------forEach----------');
+
+names.forEach(name => console.log(name));
+
+console.log('----------for----------');
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
